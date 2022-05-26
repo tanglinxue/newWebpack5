120d917797510d5382f3b7e03c0f0bf1e9295bd2
tanglinxue <31237473+tanglinxue@users.noreply.github.com>
2022-05-26 19:34:43 +0800

    开发

--- src/main.js
+++ src/main.js
@@ -2,3 +2,12 @@ import count from './js/count';
 import sum from './js/sum'
 console.log(count(2,1))
 console.log(sum(1,2,3,4))
+// 想要webpack打包资源，必须引入该资源
+import "./css/iconfont.css";
+import "./css/index.css";
+import "./less/index.less";
+import "./sass/index.sass";
+import "./sass/index.scss";
+import "./stylus/index.styl";
+let result = count(2,1)
+console.log(result)
